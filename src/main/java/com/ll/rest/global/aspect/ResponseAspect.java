e819c8bbe2a98d9a27bdfd987cea5ea6aa35e07a
kimhyeji <gpwl5589@gmail.com>
2025-02-10 22:46:08 +0900

    response entity remove 5

--- src/main/java/com/ll/rest/global/aspect/ResponseAspect.java
+++ src/main/java/com/ll/rest/global/aspect/ResponseAspect.java
@@ -1,4 +1,5 @@
 package com.ll.rest.global.aspect;
+import com.ll.rest.global.rsData.RsData;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.aspectj.lang.ProceedingJoinPoint;
@@ -34,7 +35,12 @@ public class ResponseAspect {
             """)
     public Object handleResponse(ProceedingJoinPoint joinPoint) throws Throwable {
         Object proceed = joinPoint.proceed();
-        response.setStatus(201);
+
+        if (proceed instanceof RsData<?>) {
+            RsData<?> rsData = (RsData<?>) proceed;
+            response.setStatus(rsData.getStatusCode());
+        }
+
         return proceed;
     }
 }
