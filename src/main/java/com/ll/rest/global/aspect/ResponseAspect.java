b4484a78e0c82400ef1383ac6fc27aa87e51da4b
kimhyeji <gpwl5589@gmail.com>
2025-02-10 22:45:29 +0900

    response entity remove 4

--- src/main/java/com/ll/rest/global/aspect/ResponseAspect.java
+++ src/main/java/com/ll/rest/global/aspect/ResponseAspect.java
@@ -1,4 +1,5 @@
 package com.ll.rest.global.aspect;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Component;
 @Component
 @RequiredArgsConstructor
 public class ResponseAspect {
+    private final HttpServletResponse response;
     @Around("""
             (
                 within
@@ -23,15 +25,16 @@ public class ResponseAspect {
                     @annotation(org.springframework.web.bind.annotation.PutMapping)
                     ||
                     @annotation(org.springframework.web.bind.annotation.DeleteMapping)
+                    ||
+                    @annotation(org.springframework.web.bind.annotation.RequestMapping)
                 )
             )
             ||
             @annotation(org.springframework.web.bind.annotation.ResponseBody)
             """)
     public Object handleResponse(ProceedingJoinPoint joinPoint) throws Throwable {
-        System.out.println("handleResponse 시작");
         Object proceed = joinPoint.proceed();
-        System.out.println("handleResponse 끝");
+        response.setStatus(201);
         return proceed;
     }
 }
